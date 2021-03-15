7523f80c2e78b89b3b2051aad6df32893451bd68
unknown <swernigge123@gmail.com>
2021-03-15 12:21:10 +0100

    changes

--- test1.cpp
+++ test1.cpp
@@ -8,4 +8,19 @@ template<typename T>
 class Iterator : public Base_Iterator
 {
    T iter; 
+   public:
+      Iterator(T iter) { this -> iter = iter; }
+      ~Iterator(){}
+
+      bool operator != (Iterator<T> rhs) { return iter != other.iter; }
+      template<typename U>
+      bool operator != (Iterator<U> rhs) { return true; }
+      auto operator * () { return *iter; }
+      void operator ++ () { ++iter; }
+};
+
+template<std::size_t S>
+class Packed_Iterator
+{
+   std::array<std::shared_ptr<Base_Iterator*>, S> pack;
 };
